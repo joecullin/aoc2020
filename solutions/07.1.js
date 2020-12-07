0c98aa3ea134b7384b1ea7ddfec6d6f582927dee
Joe Cullin <joe@joecullin.com>
2020-12-07 08:44:26 -0500

    Day 7 solutions

---
+++ solutions/07.1.js
@@ -0,0 +1,40 @@
+const logger = require("../common/logger-simple");
+const {readLines} = require("../common/readInput");
+
+
+const run = async (params) => {
+    const input = await readLines({inputPath: params.inputPath});
+    let containedBy = {};
+    input.forEach(line => {
+        if (/contain \d/.test(line)){
+            // shiny chartreuse bags contain 3 bright brown bags, 2 dim cyan bags, 4 shiny brown bags, 1 clear black bag.
+            const parts = line.replace(/ bags?/g, '').replace(/, /g, ',').split(' contain ');
+            const contain = parts[1].replace(/\d+\s+/g, '').split(/[,.]/).filter(part => part !== "");
+            contain.forEach(bagType => {
+                if (!containedBy[bagType]){
+                    containedBy[bagType] = [];
+                }
+                containedBy[bagType].push(parts[0]);
+            });
+        }
+    });
+
+    const getContainers = (startingType) => {
+        let containers = containedBy[startingType];
+        if (!containers){
+            return [];
+        }
+        containers.forEach(containerType => {
+            const nextLevel = getContainers(containerType);
+            if (nextLevel && nextLevel.length){
+                Array.prototype.push.apply(containers, nextLevel);
+            }
+        });
+        return containers; 
+    };
+
+    const answer = (new Set(getContainers("shiny gold"))).size;
+    console.log("Answer: " + answer);
+};
+
+module.exports = { run };
---
+++ solutions/07.2.js
@@ -0,0 +1,39 @@
+const logger = require("../common/logger-simple");
+const {readLines} = require("../common/readInput");
+
+const run = async (params) => {
+    const input = await readLines({inputPath: params.inputPath});
+    let mustContain = {};
+    input.forEach(line => {
+        if (/contain \d/.test(line)){
+            // shiny chartreuse bags contain 3 bright brown bags, 2 dim cyan bags, 4 shiny brown bags, 1 clear black bag.
+            const parts = line.replace(/ bags?/g, '').replace(/, /g, ',').split(' contain ');
+            const contain = parts[1].split(/[,.]/).filter(part => part !== "");
+            let contents = {};
+            contain.forEach(bagSpec => {
+                const [_, number, type] = bagSpec.match(/^(\d+) (.*)/);
+                contents[type] = parseInt(number);
+            });
+            mustContain[parts[0]] = contents;
+        }
+    });
+
+    const getContents = (startingType) => {
+        let count = 0;
+        let containers = mustContain[startingType];
+        if (!containers){
+            return count;
+        }
+        Object.entries(containers).forEach( ([containerType, quantity]) => {
+            count += quantity;
+            const nextLevel = getContents(containerType);
+            count += (quantity * nextLevel);
+        });
+        return count; 
+    };
+
+    const answer = getContents("shiny gold");
+    console.log("Answer: " + answer);
+};
+
+module.exports = { run };
